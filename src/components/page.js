3653905127d54099468a9d154235b1674d7c6821
kargjonas <jonas.karg@hotmail.com>
2019-03-28 20:45:48 +0100

    STarting to implement some scroll stuff

--- src/components/page.js
+++ src/components/page.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Boxes from "./boxes";
+import style from "../style/page.scss";
 
-export default function Page() {
-  return (
-    <div>
-      <Boxes />
-      {/* Test */}
-    </div>
-  );
+export default class Page extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  // componentDidMount() {
+  //   this.refs.boxes.addEventListener("scroll", (e) => {
+  //     console.log(e);
+  //   });
+  // }
+
+  render() {
+    return (
+      <div style={style}>
+        <div id="box-scroll">
+          <div id="boxes">
+            <Boxes />
+          </div>
+        </div>
+        Test
+      </div>
+    );
+  }
 }
